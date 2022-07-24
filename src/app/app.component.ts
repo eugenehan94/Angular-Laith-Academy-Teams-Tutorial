e93a5e156d590f74460436a23251e0e8514ee4c6
eugenehan94 <eugene.han94@gmail.com>
2022-07-24 14:45:51 -0400

    added methods and used structural derivative

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,5 +9,12 @@ export class AppComponent {
   newMemberName = "";
   members: string[] = [];
 
-  
+  onInput(member: string){
+    this.newMemberName = member;
+    console.log(this.newMemberName)
+  }
+
+  addMember(){
+    this.members.push(this.newMemberName)
+  }  
 }
