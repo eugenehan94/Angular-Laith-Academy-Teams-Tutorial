b19ae1e0ccc6622e4ee647484bf643da3180684c
eugenehan94 <eugene.han94@gmail.com>
2022-07-25 17:58:13 -0400

    completed tutorial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,13 +8,64 @@ import { Component } from '@angular/core';
 export class AppComponent {
   newMemberName = "";
   members: string[] = [];
+  errorMessage = "";
+  // this is also acceptable
+  // numberOfTeams: number | string = "";
+  numberOfTeams: number | "" = "";
+  teams: string[][] = []
+
 
   onInput(member: string){
     this.newMemberName = member;
     console.log(this.newMemberName)
   }
 
+  onNumberOfTeamsInput(value: string){
+    this.numberOfTeams = Number(value)
+  }
+
   addMember(){
+    if (!this.newMemberName){
+      this.errorMessage = "Name can't be empty"
+      return;
+    }
+    this.errorMessage = ""
     this.members.push(this.newMemberName)
+    this.newMemberName=""
   }  
+
+  generateTeams(){
+
+    if(!this.numberOfTeams || this.numberOfTeams <= 0){
+      this.errorMessage = "Invalid number of teams"
+      return
+    }
+
+    if (this.members.length < this.numberOfTeams){
+      this.errorMessage = "Not enough members"
+      return
+    }
+
+    this.errorMessage = ""
+    // Creates a copy of the members array as above to prevent altering of the members array
+    // for this logic
+    const allMembers = [...this.members]
+
+    while(allMembers.length){
+      for(let i = 0; i < this.numberOfTeams; i++){
+        const randomIndex = Math.floor(Math.random() * allMembers.length )
+        const member = allMembers.splice(randomIndex, 1)[0]
+        if (!member ) break;
+        if (this.teams[i]){
+          this.teams[i].push(member)
+        }else {
+          this.teams[i] = [member]
+        }
+      }
+    }
+
+    this.members = [];
+    this.numberOfTeams = "";
+
+  }
 }
